f4da27d8f3c0feb0cb584b07cc89d4218e23f710
Viral-Sachde <133246827+Viral-Sachde@users.noreply.github.com>
2024-01-05 14:42:08 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,7 +88,8 @@ export default function Page() {
           </div>
 
           <Avatar className="h-28 w-28">
-            <AvatarImage alt={RESUME_DATA.name} src={RESUME_DATA.avatarUrl} />
+            <AvatarImage alt={RESUME_DATA.name} src="../images/logos/photo.jpg" />
+            
             <AvatarFallback>{RESUME_DATA.initials}</AvatarFallback>
           </Avatar>
         </div>
